e889186e80184fb2b5a5d5375bdf480a2e50e2f5
prateekpranveer <55608916+prateekpranveer@users.noreply.github.com>
2022-07-10 10:45:29 -0700

    Delete BT && BST/BT && BST directory

--- BT && BST/BT && BST/Root_to_Leaf_Path_Sum.cpp
+++
@@ -1,6 +0,0 @@
-bool hasPathSum(Node *root, int S) {
-        if (!root) return root;
-        S-=root->data;
-        if (root->left == NULL && root->right==NULL) return S==0;
-        return (hasPathSum(root->left, S)|hasPathSum(root->right, S));
-    }
